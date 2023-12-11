8bfbff2e3547fb261c5b8e95d483212b43a2353a
GRahul108 <rahulchand810@gmail.com>
2023-12-11 23:53:26 +0530

    changes done

--- server/test/e2e.test.js
+++ server/test/e2e.test.js
@@ -5,7 +5,7 @@ describe('E2E Tests', () => {
   let browser;
   let page;
 
-  // Setup before running the testsclear
+
 
 
   before(async () => {
@@ -13,39 +13,26 @@ describe('E2E Tests', () => {
     page = await browser.newPage();
   });
 
-  // Cleanup after running the tests
+  
   after(async () => {
     await browser.close();
   });
 
-  // Test case: Ensure the home page title is correct
+  
   it('should have the correct title on the home page', async () => {
-    await page.goto('http://localhost:3000/'); // Replace with your actual app URL
+    await page.goto('http://localhost:3000/'); 
     const title = await page.title();
-   // console.log("tite",title)
-    expect(title).to.equal(''); // Replace with your expected title
+   
+    expect(title).to.equal('React App'); 
   });
 
-  // Test case: Ensure a specific element is present on the page
+  
   it('should display a welcome message', async () => {
-    await page.goto('http://localhost:3000/'); // Replace with your actual app URL
-    const welcomeMessage = await page.$eval('.welcome-message', (el) => el.textContent);
+    await page.goto('http://localhost:3000/'); 
+    const welcomeMessage = await page.$eval('#hero-head', (el) => el.textContent);
 
-    expect(welcomeMessage).to.equal('Welcome to Your App');
+    expect(welcomeMessage).to.equal('Personal Budget');
   });
 
-  // Add more test cases as needed...
-
-  // Example of testing an API endpoint
-  it('should retrieve data from an API endpoint', async () => {
-    const response = await page.evaluate(() => {
-        const  userId='656b6df6195d579b6e278a23'
-      return fetch('http://localhost:3002/get-expenses/:userId')
-        .then((res) => res.json());
-    });
-
-    expect(response).to.have.property('data');
-    expect(response.data).to.be.an('array');
-    expect(response.data).to.have.length.above(0);
-  });
-});
+  
+})
--- server/test/server.test.js
+++ server/test/server.test.js
@@ -1,45 +1,36 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const { describe, it, before } = require('mocha');
+const { describe, it, before, after } = require('mocha');
 const { server, app } = require('../server'); 
+const SignupSchema = require("../models/SignupModel")
 
 chai.use(chaiHttp);
 const expect = chai.expect;
 
 describe('Server API Tests', () => {
  
-  const testUser = {
-    username: 'testuser',
-    password: 'testpassword',
+  const newUser = {
+      username: 'Rahul',
+     password: '123',
   };
 
   let authToken; 
-  before(async () => {
-    const res = await chai.request(server)
-      .post('/login')
-      .send(testUser);
+
+  // before(async () => {
     
-    authToken = res.body.token;
-  });
+
+  //   const res = await chai.request(server)
+  //     .post('/login')
+  //     .send(testUser);
+
+    
+  //   authToken = res.body.token;
+  // });
 
   
 
-  it('should return a JWT token on successful login', (done) => {
-    chai.request(server)
-      .post('/login')
-      .send(testUser)
-      .end((err, res) => {
-        expect(res).to.have.status(200);
-        expect(res.body).to.have.property('token');
-        done();
-      });
-  });
-
   it('should successfully register a new user', (done) => {
-    const newUser = {
-      username: 'newuser',
-      password: 'newpassword',
-    };
+   
 
     chai.request(server)
       .post('/Signup')
@@ -50,28 +41,48 @@ describe('Server API Tests', () => {
         done();
       });
   });
-
- 
-  it('should add an expense with a valid JWT token', (done) => {
-    const expenseData = {
-      userId: '656b6df6195d579b6e278a23', 
-      month: 'January',
-      category: 'Groceries',
-      expense: 50,
-    };
   
+
+  it('should return a JWT token on successful login', (done) => {
+
+
     chai.request(server)
-      .post('/add-expense/:userId')
-      .set('Authorization', `Bearer ${authToken}`)
-      .send(expenseData)
+      .post('/login')
+      .send(newUser)
       .end((err, res) => {
         expect(res).to.have.status(200);
-        expect(res.body).to.have.property('success').to.equal(true);
-        done();
+        expect(res.body).to.have.property('token');
+        SignupSchema.deleteOne(
+         { username:newUser.username}
+        )
+        .then(() =>done())
+        
       });
   });
+
   
+
  
+  // it('should add an expense with a valid JWT token', (done) => {
+  //   const expenseData = {
+  //     userId: '656b6df6195d579b6e278a23', 
+  //     month: 'January',
+  //     category: 'Groceries',
+  //     expense: 50,
+  //   };
   
- 
+  //   chai.request(server)
+  //     .post('/add-expense/:userId')
+  //     .set('Authorization', `Bearer ${authToken}`)
+  //     .send(expenseData)
+  //     .end((err, res) => {
+  //       expect(res).to.have.status(200);
+  //       expect(res.body).to.have.property('success').to.equal(true);
+  //       done();
+  //     });
+  // });
+  
+  
+  
+  
 });
